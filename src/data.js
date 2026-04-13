ebb62da01e60d7cac20288c70e0e59e4410b93c3
MJona95 <mjona9514@gmail.com>
2026-04-13 10:52:17 -0600

    images optimization

--- src/data.js
+++ src/data.js
@@ -1,9 +1,11 @@
-export const data = [
-     infoCardA = [
-        {
-            icon: "motorbike.svg",
-            title: "Ofreciendoles",
-            paragraph: "Nos encargamos de entregarle su medicamento hasta la puerta de su hogar, asegurando su      pronta llegada. Es completamente gratis en la ciudad de somoto y sectores aledaños"
-        }
-    ]
-]
+import motorbikeIcon from "../public/motorbike.svg";
+
+export const infoCardA = [
+    {
+        icon: motorbikeIcon,
+        title: "Ofreciendoles",
+        paragraph: "Nos encargamos de entregarle su medicamento hasta la puerta de su hogar, asegurando su pronta llegada. Es completamente gratis en la ciudad de somoto y sectores aledaños"
+    }
+];
+
+export const data = [infoCardA];
